dc27f5e125b88c95e9f6a2158b1fefbb826165a6
Maxim Korovin <totsamax@gmail.com>
2019-03-10 15:40:54 +0300

    Первый коммит Передалал с нуля

---
+++ src/components/FirstCup.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./FirstCup.scss";
+const FirstCup = () => {
+  return (
+    <div>
+      <h1>Первый кофе всего за 1₽</h1>
+      <p>
+        Выберете ваш любимый напиток и мы угостим вас по символической цене. Мы
+        уверены, что вы вернетесь
+      </p>
+    </div>
+  );
+};
+export default FirstCup;
---
+++ src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+          <NavbarToggler onClick={this.toggle} />
+          <NavbarBrand href="/" className="logo text-center">22Cusp</NavbarBrand>
+        </Navbar>
+      </div>
+    );
+  }
+}
