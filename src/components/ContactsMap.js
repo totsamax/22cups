0d0e13a3c0a113bf1fc7fe952afa2afbf6927006
Maxim Korovin <totsamax@gmail.com>
2019-03-27 21:37:38 +0300

    q

--- src/components/ContactsMap.js
+++ src/components/ContactsMap.js
@@ -1,64 +1,79 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Map, Marker, MarkerLayout } from "yandex-map-react";
-import { UncontrolledCollapse, Button, CardBody, Card } from "reactstrap";
 import "./ContactsMap.scss";
+import Modal from "react-modal";
+import SlidingPane from "react-sliding-pane";
+import "react-sliding-pane/dist/react-sliding-pane.css";
 
 class ContactMap extends Component {
   constructor(props) {
     super(props);
-    this.onEntering = this.onEntering.bind(this);
-    this.onEntered = this.onEntered.bind(this);
-    this.onExiting = this.onExiting.bind(this);
-    this.onExited = this.onExited.bind(this);
-    this.toggle = this.toggle.bind(this);
-    this.state = { collapse: false, status: "Closed" };
+    this.state = {
+      isPaneOpen: false,
+      isPaneOpenLeft: false
+    };
   }
 
-  onEntering() {
-    this.setState({ status: "Opening..." });
-  }
-
-  onEntered() {
-    this.setState({ status: "Opened" });
-    
-  }
-
-  onExiting() {
-    this.setState({ status: "Closing..." });
-  }
-
-  onExited() {
-    this.setState({ status: "Closed" });
-  }
-
-  toggle() {
-    this.setState(state => ({ collapse: !state.collapse }));
+  componentDidMount() {
+    Modal.setAppElement(this.el);
   }
 
   render() {
     return (
       <div className="mapPanel">
-        <div id="toggler">Toggle</div>
-        <UncontrolledCollapse toggler="#toggler">
-          <Map
-            onAPIAvailable={function() {
-              console.log("API loaded");
+        <div ref={ref => (this.el = ref)}>
+          <button onClick={() => this.setState({ isPaneOpen: true })}>
+            Click me to open right pane!
+          </button>
+          <div style={{ marginTop: "32px" }}>
+            <button onClick={() => this.setState({ isPaneOpenLeft: true })}>
+              Click me to open left pane with 20% width!
+            </button>
+          </div>
+          <SlidingPane
+            className="some-custom-class"
+            overlayClassName="some-custom-overlay-class"
+            isOpen={this.state.isPaneOpen}
+            title="Hey, it is optional pane title.  I can be React component too."
+            subtitle="Optional subtitle."
+            onRequestClose={() => {
+              // triggered on "<" on left top click or on outside click
+              this.setState({ isPaneOpen: false });
             }}
-            center={[55.754734, 37.583314]}
-            zoom={10}
-            width={"100%"}
-            height={"500px"}
           >
-            <Marker lat={55.783379} lon={37.775575}>
-              <MarkerLayout>
-                <div style={{borderRadius: "50%", overflow: "hidden"}}>
-                  <img src="http://loremflickr.com/30/30" />
-                </div>
-              </MarkerLayout>
-            </Marker>
-          </Map>
-        </UncontrolledCollapse>
+            <div>And I am pane content. BTW, what rocks?</div>
+            <br />
+            <img src="img.png" />
+          </SlidingPane>
+          <SlidingPane
+            closeIcon={<div>Some div containing custom close icon.</div>}
+            isOpen={this.state.isPaneOpenLeft}
+            title="Hey, it is optional pane title.  I can be React component too."
+            from="bottom"
+            width="100%"
+            onRequestClose={() => this.setState({ isPaneOpenLeft: false })}
+          >
+            <Map
+              onAPIAvailable={function() {
+                console.log("API loaded");
+              }}
+              center={[55.754734, 37.583314]}
+              zoom={10}
+              width={"100%"}
+              height={"500px"}
+            >
+              <Marker lat={55.783379} lon={37.775575}>
+                <MarkerLayout>
+                  <div style={{ borderRadius: "50%", overflow: "hidden" }}>
+                    <img src="http://loremflickr.com/30/30" />
+                  </div>
+                </MarkerLayout>
+              </Marker>
+            </Map>
+          </SlidingPane>
+        </div>
+        ;
       </div>
     );
   }
